4402b0bc06a83b96284cecd041dd6360bdb90266
paulcacheux <paulcacheux@gmail.com>
2020-03-18 04:45:14 +0000

    Deploying to gh-pages from  - 2d47e135397c60115264f1b488b88286f58ee368 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/check-splitter/precache-manifest.b257e332ecae74e2affb830d0b5ca355.js"
+  "/check-splitter/precache-manifest.29c637c747479834f6c4fed91a8f7e60.js"
 );
 
 self.addEventListener('message', (event) => {
